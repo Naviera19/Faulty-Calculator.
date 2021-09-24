aafbe63ec37f06765e9ea9601dac1b220b3c8fbe
Naviera19 <91308602+Naviera19@users.noreply.github.com>
2021-09-24 13:34:27 +0530

    Add files via upload

---
+++ Faulty calculator.py
@@ -0,0 +1,34 @@
+#Design a calculator which can solve all problems other than 45*3=555, 56+9=77, 56/6=4.
+while(True):
+    print("Enter the no. 1: ")
+    v1 = int(input())
+    print("Enter the no. 2: ")
+    v2 = int(input())
+    print("Enter the operator: ")
+    v3 = input()
+
+    if v1 == 45 and v2 == 3 and v3 == '*':
+        print("555")
+    elif v1 == 56 and v2 == 9 and v3 == '+':
+        print("77")
+    elif v1 == 56 and v2 == 6 and v3 == '/':
+        print("4")
+    elif v3 == '*':
+        print(int(v1) * int(v2))
+    elif v3 == '+':
+        print(int(v1) + int(v2))
+    elif v3 == '-':
+        print(int(v1) - int(v2))
+    elif v3 == '/':
+        print(int(v1) / int(v2))
+    elif v3 == '%':
+        print(int(v1) % int(v2))
+    else:
+        print("Sorry! This isn't right.")
+
+    ques = input("Do you want to calculate again? Type Y or N.")
+    if ques == 'Y':
+        continue
+    if ques == 'N':
+        print("You exited.")
+        break
