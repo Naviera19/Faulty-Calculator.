b1fce9ef275f0ba5ead60a81abb9c0a80eefe19b
Naviera19 <91308602+Naviera19@users.noreply.github.com>
2021-09-24 13:39:16 +0530

    Update Faulty calculator.py

--- Faulty calculator.py
+++ Faulty calculator.py
@@ -1,4 +1,7 @@
-#Design a calculator which can solve all problems other than 45*3=555, 56+9=77, 56/6=4.
+'''Design a calculator using Python which can solve all problems other than 45*3=555, 56+9=77, 56/6=4. 
+The calculator must keep on calculating according to the user's commands. 
+I've used while loop with break and continue statements'''
+
 while(True):
     print("Enter the no. 1: ")
     v1 = int(input())
